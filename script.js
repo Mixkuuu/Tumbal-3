121f5a6eb6fed19172b751df7bd3fe8e97a2be0b
Mixkuuu <auahk398@gmail.com>
2026-03-10 20:42:11 +0700

    Add example functions for supervised and unsupervised learning

---
+++ script.js
@@ -0,0 +1,13 @@
+function contohSupervised(){
+
+document.getElementById("hasil").innerHTML =
+"Contoh: deteksi email spam, prediksi harga rumah, dan pengenalan wajah.";
+
+}
+
+function contohUnsupervised(){
+
+document.getElementById("hasil").innerHTML =
+"Contoh: pengelompokan pelanggan, analisis pola belanja, dan sistem rekomendasi.";
+
+}
